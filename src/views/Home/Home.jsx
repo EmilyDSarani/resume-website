f6dbcd2d5bf5c49b77ffd09f0e0d028939e80d07
EmilyDSarani <emily.sarani@gmail.com>
2022-05-31 15:34:24 -0500

    home page done

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -12,24 +12,29 @@ export default class Home extends Component {
       <div className="flex items-center justify-center h-screen w-screen flex-col">
         <ParticlesMagic />
         {/* the brackets in tailwind allows you to use arbitary values */}
-        <div className="flex z-[9999] w-4/5 h-4/5 max-w-4xl justify-center bg-gradient-to-r from-sky-800 to-transparent backdrop-blur-sm shadow flex-col items-center">
+        <div className="flex z-[9999] w-5/6 sm:w-4/5 h-5/6 sm:h-4/5 max-w-4xl justify-center bg-gradient-to-r from-sky-800 to-transparent backdrop-blur-sm shadow flex-col items-center">
 
-          <img className={styles.me} src={me} alt="my avatar" />
-          <h1 className='text-red-700'>Emily Sarani (She/Her)</h1>
-          <p className={styles.bio}>FullStack JavaScript Developer</p>
-          <p className={styles.bio}>
+          <img className='h-[200px] w-[200px] p-2.5' src={me} alt="my avatar" />
+          
+
+          <h1 className='text-slate-300 text-3xl text-center pb-3 font-mono'>Emily Sarani (She/Her)</h1>
+       
+          <p className='text-emerald-500 font-mono text-xl pb-2 text-center'>FullStack JavaScript Developer</p>
+          <p className='text-emerald-500 font-mono text-xl pb-2 text-center'>
             {" "}
             Currently involved with Code for America, Co.Lab and The Collab Lab
           </p>
-          <div className={styles.buttonContainer}>
+         
+          <div className='flex flex-col sm:flex-row justify-center sm:justify-between items-center w-[400px] pt-4'>
+
             <Link to="/portfolio">
-              <button>My Portfolio</button>
+              <button className='flex items-center justify-center border-2 border-sky-400 rounded-full p-2 bg-slate-700 text-slate-300 hover:border-slate-500 hover:bg-slate-900 hover:text-emerald-400'>My Portfolio</button>
             </Link>
             <Link to="/experience">
-              <button>My Experience</button>
+              <button className='flex items-center justify-center border-2 border-sky-400 rounded-full p-2 bg-slate-700 text-slate-300 hover:border-slate-500 hover:bg-slate-900 hover:text-emerald-400'>My Experience</button>
             </Link>
             <Link to="/contact">
-              <button>Connect With Me</button>
+              <button className='flex items-center justify-center border-2 border-sky-400 rounded-full p-2 bg-slate-700 text-slate-300 hover:border-slate-500 hover:bg-slate-900 hover:text-emerald-400'>Connect With Me</button>
             </Link>
           </div>
           {/* <Name /> */}
