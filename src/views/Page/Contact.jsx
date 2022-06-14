912d9fe542cf99c693293fb27b2e9957b8745b10
EmilyDSarani <emily.sarani@gmail.com>
2022-06-14 14:23:23 -0500

    border on hover around image

--- src/views/Page/Contact.jsx
+++ src/views/Page/Contact.jsx
@@ -19,20 +19,26 @@ export default function Contact() {
         {/* display: flex;
     justify-content: space-between;
     width: 600px; */}
-        <div className={buttonContainerStyles}>
+        <div className='flex flex-col sm:flex-row justify-center sm:justify-between items-center w-[400px] pt-4'>
+        <div className='hover:border-2 border-double rounded-2xl border-emerald-300'>
         <a href="https://twitter.com/emilysarani" target="_blank" rel="noreferrer" >
         <img className='h-[100px] w-[100px] p-1.5'src={twitter} alt='icon created by Pixel Perfect'/>
         <span className='sr-only'>twitter link</span>
         </a>
+        </div>
 
+        <div className='hover:border-2 border-double rounded-2xl border-emerald-300'>
         <a href="https://www.linkedin.com/in/emily-sarani/" target="_blank" rel="noreferrer" ><img className='h-[100px] w-[100px] p-2.5"'src={linkedIn} alt='icon created by Pixel Perfect'/> 
         <span className='sr-only'>linkedin link</span>
         </a>
+        </div>
 
+        <div className='hover:border-2 border-double rounded-2xl border-emerald-300'>
         <a href="https://github.com/EmilyDSarani" target="_blank" rel="noreferrer" >
         <img className='h-[100px] w-[100px] p-2.5"'src={github} alt='icon created by Pixel Perfect'/>
         <span className='sr-only'>github link</span>
         </a>
+        </div>
 
         </div>
 
